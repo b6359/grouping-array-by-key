ea9ecd2e6c8c41d37e847e3628c03453cf193ac4
bhadresh <jb.janjmera>
2024-06-28 18:57:14 +0530

    comments added

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
+//function for group array by key
 function groupByArray(array, key) {
-    return array.reduce((hash, obj) => {
-        if (obj[key] === undefined) return hash;
-        return Object.assign(hash, {
-            [obj[key]]: (hash[obj[key]] || []).concat(obj)
-        })
-    }, {})
+  return array.reduce((hash, obj) => {
+    if (obj[key] === undefined) return hash;
+    return Object.assign(hash, {
+      [obj[key]]: (hash[obj[key]] || []).concat(obj),
+    });
+  }, {});
 }
-module.exports = groupByArray
+module.exports = groupByArray;
